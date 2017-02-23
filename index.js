501647738bdbb9f844be1f5a238d0a3bb284fd1c
Lsong <song940@gmail.com>
2017-02-23 16:38:54 +0800

    add dir indexed pages

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ module.exports = function KelpStatic(root, options){
   var defaults = {
     index: 'index.html'
   };
-  for(var k in options){
+  for(var k in options)
     defaults[ k ] = options[ k ];
-  }
   options = defaults;
   /**
    * [function description]
@@ -26,11 +25,14 @@ module.exports = function KelpStatic(root, options){
    */
   return function(req, res, next){
     var filename = decodeURIComponent(url.parse(req.url).pathname);
-    if(filename.endsWith('/')) filename += options.index;
+    if(filename.endsWith('/') && typeof options.index === 'string') 
+      filename += options.index;
     filename = path.join(path.resolve(root), filename);
     fs.stat(filename, function(err, stat){
       if(err) return next(err);
       if(stat.isDirectory()){
+        if(options.index === true)
+          return renderDirectory(filename, res.end);
         res.writeHead(301, {
           'Location': req.url + '/'
         });
@@ -49,3 +51,32 @@ module.exports = function KelpStatic(root, options){
     });
   };
 };
+/**
+ * [renderDirectory description]
+ * @param  {[type]}   dir      [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+function renderDirectory(dir, callback){
+  var content = '', cwd = process.cwd();
+  content += '<h1>Index of '+ dir.replace(cwd, '') +'</h1>';
+  content += '<hr />';
+  fs.readdir(dir, function(err, files){
+    content += '<table width="50%">';
+    content += '<tr>';
+    content += '<td><a href="..">..</a></td>';
+    content += '</tr>';
+    files.map(function(filename){
+      var stat = fs.statSync(path.join(dir, filename));
+      content += '<tr>';
+      content += '<td><a href="' + filename + '">' + filename + '</a></td>';
+      content += '<td>' + (stat.mtime || '-')      +                '</td>';
+      content += '<td>' + (stat.size        )      +                '</td>';
+      content += '</tr>';
+    }).join('');
+    content += '</table>';
+    content += '<hr/>';
+    content += 'Powered by <a href="https://github.com/song940/kelp-static" >kelp-static</a>';
+    callback(content);
+  });
+}
